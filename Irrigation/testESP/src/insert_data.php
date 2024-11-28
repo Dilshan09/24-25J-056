abd45326b418c8667480be14b840b093afd85647
praneeth-pixzarloop <pereradilshan594@gmail.com>
2024-11-28 20:19:53 +0530

    server sending part added

---
+++ Irrigation/testESP/src/insert_data.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";  // Server name or IP address
+$username = "root";      // MySQL username
+$password = "";  // MySQL password
+$dbname = "plant_one";      // MySQL database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Get data from the POST request
+$humidity = $_POST['humidity'];
+$temperature = $_POST['temperature'];
+$light = $_POST['light'];
+
+
+
+$conn->close();
+?>
